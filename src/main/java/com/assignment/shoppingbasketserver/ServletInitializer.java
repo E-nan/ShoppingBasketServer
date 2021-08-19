4243a0f7dc4653afbcf1c27310166eb9a8ee8462
E-nan <2nan@kakao.com>
2021-08-19 20:19:49 +0900

    Initial commit

---
+++ src/main/java/com/assignment/shoppingbasketserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.assignment.shoppingbasketserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShoppingbasketserverApplication.class);
+    }
+
+}
---
+++ src/test/java/com/assignment/shoppingbasketserver/ShoppingbasketserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.shoppingbasketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingbasketserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
