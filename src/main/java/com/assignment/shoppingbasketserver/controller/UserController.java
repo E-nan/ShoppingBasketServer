1a97706a0f310c1c1144afe081d16004ab37336f
E-nan <2nan@kakao.com>
2021-08-20 23:08:34 +0900

    유저 검색 기능 추가

--- src/main/java/com/assignment/shoppingbasketserver/controller/UserController.java
+++ src/main/java/com/assignment/shoppingbasketserver/controller/UserController.java
@@ -6,6 +6,8 @@ import com.assignment.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
@@ -30,10 +32,25 @@ public class UserController {
     }
 
     /**
+     * 유저 검색 기능(파라미터 값이 없으면 전체 검색)
+     * @param user_id
+     * @return
+     */
+    @RequestMapping("/user/select")
+    public List<UserDto> selectUser(@RequestParam(required = false) String user_id){
+
+        List<UserDto> userDtoList = userDao.selectUser(user_id);
+
+        return userDtoList;
+    }
+
+    /**
      * 유저 회원탈퇴 기능
+     * @param user_no
      */
     @RequestMapping("/user/signOut")
     public void signOutUser(@RequestParam Long user_no){
+
         userDao.deleteUser(user_no);
     }
 
--- src/main/java/com/assignment/shoppingbasketserver/dao/UserDao.java
+++ src/main/java/com/assignment/shoppingbasketserver/dao/UserDao.java
@@ -4,10 +4,12 @@ package com.assignment.shoppingbasketserver.dao;
 import com.assignment.shoppingbasketserver.dto.UserDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface UserDao {
 
     void insertUser(UserDto userDto);
-
+    List<UserDto> selectUser(String userId);
     void deleteUser(Long userNo);
 }
