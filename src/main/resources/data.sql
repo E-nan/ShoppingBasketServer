46786a094a8614d19216f6becc33ba19110aad96
E-nan <2nan@kakao.com>
2021-08-21 11:49:43 +0900

    테스트 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user(user_id, user_pw, user_name, user_address) values ("enan", "1234", "정인환", "안산시")
+insert into user(user_id, user_pw, user_name, user_address) values ("hong", "1234", "홍길동", "서울시")
+insert into user(user_id, user_pw, user_name, user_address) values ("test", "1234", "테스터", "제주도")
+
+insert into item(item_name, item_price, item_description) values ("모니터", "50000", "모니터 입니다.")
+insert into item(item_name, item_price, item_description) values ("키보드", "20000", "키보드 입니다.")
+insert into item(item_name, item_price, item_description) values ("마우스", "10000", "마우스 입니다.")
