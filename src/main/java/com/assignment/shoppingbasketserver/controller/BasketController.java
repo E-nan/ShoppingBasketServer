ac518d47e735d133fb0ff93e0829386f1da02fea
E-nan <2nan@kakao.com>
2021-08-23 12:01:27 +0900

    장바구니 삭제 기능 추가 (다중삭제 기능 추가 예정)

--- src/main/java/com/assignment/shoppingbasketserver/controller/BasketController.java
+++ src/main/java/com/assignment/shoppingbasketserver/controller/BasketController.java
@@ -4,8 +4,13 @@ import com.assignment.shoppingbasketserver.dao.BasketDao;
 import com.assignment.shoppingbasketserver.dto.BasketDto;
 import com.assignment.shoppingbasketserver.vo.BasketVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/basket")
@@ -23,5 +28,25 @@ public class BasketController {
                 .build();
 
         basketDao.insertBasket(basketDto);
+    }
+
+    // 다중 검색 고려 및 데이터타입 고려
+//    @RequestMapping("/select")
+//    public List<BasketDto> selectBasket(@RequestParam Long userNo, @RequestParam(required = false) Long itemNo){
+//
+//        return basketDao.selectBasket(userNo, itemNo);
+//    }
+
+    @RequestMapping("/select")
+    public List<BasketDto> selectBasket(BasketVo basketVo){
+
+        return basketDao.selectBasket(basketVo);
+    }
+
+    // 다중 삭제 고려 수정 예정
+    @RequestMapping("/delete")
+    public void deleteBasket(BasketVo basketvo){
+
+        basketDao.deleteBasket(basketvo);
     }
 }
--- src/main/java/com/assignment/shoppingbasketserver/dao/BasketDao.java
+++ src/main/java/com/assignment/shoppingbasketserver/dao/BasketDao.java
@@ -1,6 +1,7 @@
 package com.assignment.shoppingbasketserver.dao;
 
 import com.assignment.shoppingbasketserver.dto.BasketDto;
+import com.assignment.shoppingbasketserver.vo.BasketVo;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 public interface BasketDao {
 
     void insertBasket(BasketDto basketDto);
+//    List<BasketDto> selectBasket(Long userNo, Long itemNo);
+    List<BasketDto> selectBasket(BasketVo basketVo);
+    void deleteBasket(BasketVo basketVo);
 }
