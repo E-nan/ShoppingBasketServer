e1444480e092ffc47946813e672f05661a93b871
E-nan <2nan@kakao.com>
2021-08-23 14:34:03 +0900

    매퍼스캔 등록

--- src/main/java/com/assignment/shoppingbasketserver/ShoppingbasketserverApplication.java
+++ src/main/java/com/assignment/shoppingbasketserver/ShoppingbasketserverApplication.java
@@ -1,9 +1,11 @@
 package com.assignment.shoppingbasketserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.assignment.shoppingbasketserver.dao")
 public class ShoppingbasketserverApplication {
 
     public static void main(String[] args) {
