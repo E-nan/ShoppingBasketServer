8005cac7d0431a48538927e084c570153dbc203b
E-nan <2nan@kakao.com>
2021-08-24 12:01:07 +0900

    aws 배포 설정 테스트

--- src/main/java/com/assignment/shoppingbasketserver/ShoppingbasketserverApplication.java
+++ src/main/java/com/assignment/shoppingbasketserver/ShoppingbasketserverApplication.java
@@ -3,10 +3,17 @@ package com.assignment.shoppingbasketserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.assignment.shoppingbasketserver.dao")
-public class ShoppingbasketserverApplication {
+public class ShoppingbasketserverApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(ShoppingbasketserverApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingbasketserverApplication.class, args);
